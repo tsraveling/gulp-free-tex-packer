63ebf44be60507241d44a791dc403a97be35fa0c
Tim Raveling <tsraveling@gmail.com>
2025-02-18 16:40:09 -0700

    Updated package settings

--- index.js
+++ index.js
@@ -5,69 +5,69 @@ let texturePacker = require("free-tex-packer-core");
 let appInfo = require("./package.json");
 
 function fixPath(path) {
-    return path.split("\\").join("/");
+  return path.split("\\").join("/");
 }
 
 function getExtFromPath(path) {
-    return path.split(".").pop().toLowerCase();
+  return path.split(".").pop().toLowerCase();
 }
 
 function getError(txt) {
-    return new PluginError(appInfo.name, txt)
+  return new PluginError(appInfo.name, txt)
 }
 
 const SUPPORTED_EXT = ["png", "jpg", "jpeg"];
 
-module.exports = function (options) {
-    let files = [];
-    let firstFile = null;
+module.exports = function(options) {
+  let files = [];
+  let firstFile = null;
 
-    function bufferContents(file, enc, cb) {
-        if (file.isNull()) {
-            cb();
-            return;
-        }
-
-        if (file.isStream()) {
-            cb(getError("Streaming not supported"));
-            return;
-        }
-
-        if (!firstFile) firstFile = file;
-
-        if (SUPPORTED_EXT.indexOf(getExtFromPath(file.relative)) < 0) {
-            cb();
-            return;
-        }
-
-        files.push({ path: fixPath(file.relative), contents: file.contents });
-
-        cb();
+  function bufferContents(file, _enc, cb) {
+    if (file.isNull()) {
+      cb();
+      return;
     }
 
-    function endStream(cb) {
-        if (!files.length) {
-            cb();
-            return;
-        }
-
-        if (!options) options = {};
-        options.appInfo = appInfo;
-
-        texturePacker(files, options, (files, error) => {
-            if (error) {
-                cb(getError(error.message || error.description || "Unknown error"));
-                return;
-            }
-            for (let item of files) {
-                let file = firstFile.clone({ contents: false });
-                file.path = path.join(firstFile.base, item.name);
-                file.contents = item.buffer;
-                this.push(file);
-            }
-            cb();
-        });
+    if (file.isStream()) {
+      cb(getError("Streaming not supported"));
+      return;
     }
 
-    return through.obj(bufferContents, endStream);
+    if (!firstFile) firstFile = file;
+
+    if (SUPPORTED_EXT.indexOf(getExtFromPath(file.relative)) < 0) {
+      cb();
+      return;
+    }
+
+    files.push({ path: fixPath(file.base + '/' + file.relative) });
+
+    cb();
+  }
+
+  function endStream(cb) {
+    if (!files.length) {
+      cb();
+      return;
+    }
+
+    if (!options) options = {};
+    options.appInfo = appInfo;
+
+    texturePacker(files, options, (files, error) => {
+      if (error) {
+        cb(getError(error.message || error.description || "Unknown error"));
+        return;
+      }
+      for (let item of files) {
+        let file = firstFile.clone({ contents: false });
+        file.path = path.join(firstFile.base, item.name);
+        file.contents = item.buffer;
+        this.push(file);
+      }
+      cb();
+    });
+  }
+
+  return through.obj(bufferContents, endStream);
 };
