e22b0b70346d62caf518da2c568dbe6497dc7e32
Tim Raveling <tsraveling@gmail.com>
2025-02-23 11:55:23 -0700

    Revert "Started breaking out multiple folder support"
    This reverts commit 020e47fb4de5599491c68bdeaa1790c0cef02fbf.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let through = require("through2");
 let path = require("path");
-let PluginError = require("plugin-error")
 let texturePacker = require("@tsraveling/free-tex-packer-core");
 let appInfo = require("./package.json");
 
@@ -58,44 +57,19 @@ module.exports = function(options) {
     if (!options) options = {};
     options.appInfo = appInfo;
 
-    let folderStructure = {}
-
-    files.forEach(file => {
-      let parts = file.relative.split("/")
-      let filename = parts.pop()
-      let folder = parts.join('/')
-      if (!folderStructure[folder]) {
-        folderStructure[folder] = []
+    texturePacker(files, options, (files, error) => {
+      if (error) {
+        cb(getError(error.message || error.description || "Unknown error"));
+        return;
       }
-      file.relative = filename
-      folderStructure[folder].push(file)
-    })
-
-    let exportsLeft = Object.keys(folderStructure).length
-
-    // STUB: Set it up so that each folder outputs to its own spritesheet
-
-    Object.keys(folderStructure).forEach(path => {
-      console.log(">>> processing", path)
-      const folderFiles = folderStructure[path]
-      texturePacker(folderFiles, options, (files, error) => {
-        if (error) {
-          cb(getError(error.message || error.description || "Unknown error"));
-          return;
-        }
-        for (let item of files) {
-          let file = firstFile.clone({ contents: false });
-          file.path = path.join(firstFile.base, item.name);
-          file.contents = item.buffer;
-          this.push(file);
-        }
-        exportsLeft -= 1;
-        if (exportsLeft <= 0) {
-          cb();
-        }
-      });
-    })
-
+      for (let item of files) {
+        let file = firstFile.clone({ contents: false });
+        file.path = path.join(firstFile.base, item.name);
+        file.contents = item.buffer;
+        this.push(file);
+      }
+      cb();
+    });
   }
 
   return through.obj(bufferContents, endStream);
